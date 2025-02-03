d9e4764b086994766a781272170aebfc1266ae60
Mishab <85479103+imishab@users.noreply.github.com>
2025-02-03 11:52:26 +0530

    api endpoint linked

--- src/app.ts
+++ src/app.ts
@@ -26,11 +26,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['*', 'http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://dripside-frontend-eight.vercel.app'],
     credentials: true,
     allowedHeaders: ['Authorization', 'Content-Type'],
   })
 );
+
 
 /* IF MONGOOSE connection */
 db.connect();
